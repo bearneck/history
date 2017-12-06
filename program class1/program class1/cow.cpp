1b223cdecc1cb98cd9ab37e559954a28ba0a7499
bearneck <haogaoha@126.com>
2017-12-06 09:30:59 +0800

    Revert "Revert "母牛产仔问题""
    This reverts commit cbbc8d5384a89d5c1ec9bd89b0320161324d6d70.

---
+++ program class1/program class1/cow.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+inline long cattle(int n);
+
+void main()
+{
+	int n;
+	cout << "please input two vlues." << endl;
+	cin >> n;
+	cout << cattle(n) << endl;
+
+
+}
+
